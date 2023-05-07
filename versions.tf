9a88bce3d9487e808857c7f2f06b6fe49a15cb33
timmalstead <timmalstead@gmail.com>
2023-05-06 19:13:31 -0700

    adds github token

--- versions.tf
+++ versions.tf
@@ -17,8 +17,8 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_assume_role}"
-    session_name = "eight-lab-platform-servicemesh"
-  }
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_assume_role}"
+  #   session_name = "eight-lab-platform-servicemesh"
+  # }
 }
